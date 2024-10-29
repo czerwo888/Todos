55b6b2042f32691b7ee04d0361a7dc52406f41bf
czerwo888 <153601307+czerwo888@users.noreply.github.com>
2024-10-29 13:01:13 +0100

    Usuwanie todos

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,22 +1,54 @@
 import { Component } from '@angular/core';
-import {NgForOf, NgIf} from '@angular/common';
-import {ToDo} from '../shared/interfaces/todo.interface';
+import { NgClass, NgForOf, NgIf, NgStyle } from '@angular/common';
+import { ToDo } from '../shared/interfaces/todo.interface';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
   imports: [
     NgForOf,
-    NgIf
+    NgIf,
+    NgStyle,
+    NgClass
   ],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
-  todos : ToDo[] = [];
+  todos: ToDo[] = [];
+  selectedTodos: Set<ToDo> = new Set<ToDo>();
 
-  addTodo(todo : string) : void {
-    this.todos.push({name: todo, isComplete: true});
+  ErrorMessage: string = '';
+
+  addTodo(todo: string): void {
+    if (todo.length <= 3) {
+      this.ErrorMessage = "zadanie musi mieć więcej niż 3 znaki";
+      return;
+    }
+
+    this.ErrorMessage = '';
+    this.todos.push({ name: todo, isComplete: false });
     console.log('Aktualna lista todo: ', this.todos);
+  }
+
+  removeSelectedTodos() {
+    this.todos = this.todos.filter(todo => !this.selectedTodos.has(todo));
+    this.selectedTodos.clear();
+  }
+
+  changeTodoStatus(todo: ToDo) {
+    todo.isComplete = !todo.isComplete;
+  }
+
+  clearErrorMessage() {
+    this.ErrorMessage = '';
+  }
+
+  toggleTodoSelection(todo: ToDo) {
+    if (this.selectedTodos.has(todo)) {
+      this.selectedTodos.delete(todo);
+    } else {
+      this.selectedTodos.add(todo);
+    }
   }
 }
